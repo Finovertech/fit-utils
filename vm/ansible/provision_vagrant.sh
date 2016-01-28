a8e3f2010f03add073312710f2e4f64e1183d02f
Tom Andersen <tandersen@finovertech.coms>
2016-01-28 14:22:41 -0500

    Changed /vm/ansible to get master branch of fit-utils

--- vm/ansible/provision_vagrant.sh
+++ vm/ansible/provision_vagrant.sh
@@ -2,5 +2,5 @@
 [ -f ~/.ssh/id_rsa ] || ssh-keygen -f ~/.ssh/id_rsa -t rsa -b 4096 -N ''
 
 # Clone fit-utils to gain access to ansible scripts.
-git clone -b FAC-13_CreateDevServers --single-branch https://github.com/Finovertech/fit-utils.git
+git clone --single-branch https://github.com/Finovertech/fit-utils.git
 ansible-playbook fit-utils/ansible/Servers/download_file_archives.yml
